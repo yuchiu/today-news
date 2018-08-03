dc50a7ab0be1518596e5175db42348eb63ed62e9
yuchiu <yuchiu2002@hotmail.com>
2018-08-03 13:41:00 -0400

    tested getNews from mongodb in backend server

--- backend_server/service.py
+++ backend_server/service.py
@@ -1,7 +1,15 @@
 import pyjsonrpc
+import json
+from bson.json_util import dumps
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), './', 'utils'))
+import mongodb_client   # pylint: disable=E0401
 
 SERVER_HOST = 'localhost'
 SERVER_PORT = 4040
+
 
 class RequestHandler(pyjsonrpc.HttpRequestHandler):
     """ Test method"""
@@ -10,9 +18,16 @@ class RequestHandler(pyjsonrpc.HttpRequestHandler):
         print "add is called with %d and %d " % (a, b)
         return a + b
 
+    @pyjsonrpc.rpcmethod
+    def getNews(self):
+        db = mongodb_client.get_db()
+        news = list(db['news'].find())
+        return json.loads(dumps(news))
+
+
 http_server = pyjsonrpc.ThreadingHttpServer(
-    server_address = (SERVER_HOST, SERVER_PORT),
-    RequestHandlerClass = RequestHandler
+    server_address=(SERVER_HOST, SERVER_PORT),
+    RequestHandlerClass=RequestHandler
 )
 
 print "Strarting HTTP server on %s:%d" % (SERVER_HOST, SERVER_PORT)
