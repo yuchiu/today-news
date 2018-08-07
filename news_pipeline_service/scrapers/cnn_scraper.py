464a41bcc506a83547715b82e0475514fc047198
yuchiu <yuchiu2002@hotmail.com>
2018-08-07 19:58:24 -0400

    fixed scraper error, added test for cnn scraper

--- news_pipeline_service/scrapers/cnn_scraper.py
+++ news_pipeline_service/scrapers/cnn_scraper.py
@@ -2,46 +2,39 @@ import os
 import random
 import requests
 
-from lxml import html   # pylint: disable=E0401
+from lxml import html
 
-GET_CNN_NEWS_XPATH = '''//p[@class="zn-body__paragraph"]//text() | //div[@class="zn-body__paragraph"]//text()'''
+GET_CNN_NEWS_XPATH = """//p[contains(@class, 'zn-body__paragraph')]//text() | //div[contains(@class, 'zn-body__paragraph')]//text()"""
 
-# Load user agents
 USER_AGENTS_FILE = os.path.join(os.path.dirname(__file__), 'user_agents.txt')
 USER_AGENTS = []
 
 with open(USER_AGENTS_FILE, 'r') as uaf:
     for ua in uaf.readlines():
         if ua:
-            # remove quotation mark "
-            USER_AGENTS.append(ua.strip()[1: -1])
+            USER_AGENTS.append(ua.strip()[1:-1])
+
 random.shuffle(USER_AGENTS)
 
 
-def getHeaders():
+def _get_headers():
     ua = random.choice(USER_AGENTS)
     headers = {
-        "Connection": "close",
-        "user-Agent": ua
+        "User-Agent": ua
     }
     return headers
 
 
 def extract_news(news_url):
-    # Fetch html
     session_requests = requests.session()
-    response = session_requests.get(news_url, headers=getHeaders())
-
+    response = session_requests.get(news_url, headers=_get_headers())
     news = {}
 
     try:
-        # Parse html
         tree = html.fromstring(response.content)
-        # Extract info
         news = tree.xpath(GET_CNN_NEWS_XPATH)
         news = ''.join(news)
-    except Exception as e:
-        print e
+    except Exception:
         return {}
 
     return news
