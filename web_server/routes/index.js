77095ab6dbef09316069db7b5a4ec7b270be1ab7
yuchiu <yuchiu2002@hotmail.com>
2018-08-07 12:50:43 -0400

    fixed authPolicy on routes

--- web_server/routes/index.js
+++ web_server/routes/index.js
@@ -2,7 +2,7 @@ import { authController, newsController } from "../controllers";
 import { authPolicy } from "../utils";
 
 export default app => {
-  app.get("/api/v1/news", authPolicy.register, newsController.getNews);
-  app.post("/auth/register", authController.register);
+  app.get("/api/v1/news", newsController.getNews);
+  app.post("/auth/register", authPolicy.register, authController.register);
   app.post("/auth/login", authController.login);
 };
--- web_server/utils/authPolicy.js
+++ web_server/utils/authPolicy.js
@@ -4,7 +4,7 @@ export default {
   register: (req, res, next) => {
     const schema = {
       email: Joi.string().email(),
-      password: Joi.string().regex(new RegExp("^[a-zA-Z0-9]{6,32}$"))
+      password: Joi.string().regex(new RegExp("^[a-zA-Z0-9]{4,32}$"))
     };
     const { error, value } = Joi.validate(req.body, schema);
     if (error) {
@@ -20,7 +20,7 @@ export default {
                     <br>
                     1. It must contain ONLY the following characters: lower case, upper case, numerics
                     <br>
-                    2. It must be at least 6 characters and not greater than 32 characters.
+                    2. It must be at least 4 characters and not greater than 32 characters.
                     `
           });
           break;
