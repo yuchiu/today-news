cbeeabc1265698d611dcb5a71bda396ee2f140dd
yuchiu <yuchiu2002@hotmail.com>
2018-08-09 11:33:29 -0400

    fetch news from scrape_news_task_queue sent by news_monitor, extract article's text by scraper, then send the extracted data to deduper_task_queue

--- news_pipeline_service/news_fetcher.py
+++ news_pipeline_service/news_fetcher.py
@@ -23,3 +23,37 @@ dedupe_news_queue_client = CloudAMQPClient(
     DEDUPE_NEWS_TASK_QUEUE_URL, DEDUPE_NEWS_TASK_QUEUE_NAME)
 scrape_news_queue_client = CloudAMQPClient(
     SCRAPE_NEWS_TASK_QUEUE_URL, SCRAPE_NEWS_TASK_QUEUE_NAME)
+
+
+def handle_message(msg):
+    if msg is None or not isinstance(msg, dict):
+        print('message is broken')
+        return
+
+    task = msg
+    text = None
+
+    article = Article(task['url'])
+    article.download()
+    article.parse()
+
+    task['text'] = article.text
+    dedupe_news_queue_client.sendMessage(task)
+
+
+def run():
+    while True:
+        if scrape_news_queue_client is not None:
+            msg = scrape_news_queue_client.getMessage()
+            if msg is not None:
+                # parse and process the task
+                try:
+                    handle_message(msg)
+                except Exception as e:
+                    print(e)
+                    pass
+            scrape_news_queue_client.sleep(SLEEP_TIME_IN_SECONDS)
+
+
+if __name__ == "__main__":
+    run()
