c952f21e01dad64df84abb7b9ae7ff7b5246f55d
yuchiu <yuchiu2002@hotmail.com>
2018-08-10 14:12:12 -0400

    use tfidf to dedupe duplicated news from dedupe_queue, store data into mongodb after news is validated

--- news_pipeline_service/news_deduper.py
+++ news_pipeline_service/news_deduper.py
@@ -23,6 +23,51 @@ cloudAMQP_client = CloudAMQPClient(
     DEDUPE_NEWS_TASK_QUEUE_URL, DEDUPE_NEWS_TASK_QUEUE_NAME)
 
 
+def handle_message(msg):
+    if msg is None or not isinstance(msg, dict):
+        return
+    task = msg
+    text = task['text']
+    if text is None:
+        return
+
+    # get all recent news based on publishedAt
+    published_at = parser.parse(task['publishedAt'])
+    published_at_day_begin = datetime.datetime(
+        published_at.year, published_at.month, published_at.day, 0, 0, 0, 0)
+    published_at_day_end = published_at_day_begin + datetime.timedelta(days=1)
+
+    db = mongodb_client.get_db()
+    same_day_news_list = list(db[NEWS_TABLE_NAME].find(
+        {'publishedAt': {'$gte': published_at_day_begin, '$lt': published_at_day_end}}))
+
+    print("same_day_news_list" + str(same_day_news_list))
+
+    if same_day_news_list is not None and len(same_day_news_list) > 0:
+        print("==== in same_day_news_list ====")
+        documents = [news['text'] for news in same_day_news_list]
+        documents.insert(0, text)
+
+        # Calculate similarity matrix
+        tfidf = TfidfVectorizer().fit_transform(documents)
+        pairwise_sim = tfidf * tfidf.T
+
+        print(pairwise_sim)
+
+        rows, _ = pairwise_sim.shape
+
+        for row in range(1, rows):
+            if pairwise_sim[row, 0] > SAME_NEWS_SIMILARITY_THRESHOLD:
+                # Duplicated news. Ignore.
+                print("Duplicated news. Ignore.")
+                return
+
+    task['publishedAt'] = parser.parse(task['publishedAt'])
+
+    db[NEWS_TABLE_NAME].replace_one(
+        {'digest': task['digest']}, task, upsert=True)
+
+
 def run():
     while True:
         if cloudAMQP_client is not None:
