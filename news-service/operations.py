852c39592016b16a80267fac98f79ad532d9c7a1
yuchiu <yuchiu2002@hotmail.com>
2018-11-23 10:55:08 -0500

    fixed news-service no userId errors

--- news-service/operations.py
+++ news-service/operations.py
@@ -59,25 +59,26 @@ def getNewsSummariesForUser(user_id, page_num):
     end_index = page_num * NEWS_LIST_BATCH_SIZE
 
     sliced_news = []
+    if(user_id):
+        if redis_client.get(user_id) is not None:
+            total_news_digests = pickle.loads(redis_client.get(user_id))
 
-    if redis_client.get(user_id) is not None:
-        total_news_digests = pickle.loads(redis_client.get(user_id))
-
-        # If begin_index is out of range, this will return empty list;
-        # If end_index is out of range (begin_index is within the range), this
-        # will return all remaining news ids.
-        sliced_news_digests = total_news_digests[begin_index:end_index]
-        db = mongodb_client.get_db()
-        sliced_news = list(db[DB_NEWS_TABLE_NAME].find(
-            {'digest': {'$in': sliced_news_digests}}))
+            # If begin_index is out of range, this will return empty list;
+            # If end_index is out of range (begin_index is within the range), this
+            # will return all remaining news ids.
+            sliced_news_digests = total_news_digests[begin_index:end_index]
+            db = mongodb_client.get_db()
+            sliced_news = list(db[DB_NEWS_TABLE_NAME].find(
+                {'digest': {'$in': sliced_news_digests}}))
     else:
         db = mongodb_client.get_db()
         total_news = list(db[DB_NEWS_TABLE_NAME].find().sort(
             [('publishedAt', -1)]).limit(NEWS_LIMIT))
         total_news_digests = [x['digest'] for x in total_news]
 
-        redis_client.set(user_id, pickle.dumps(total_news_digests))
-        redis_client.expire(user_id, USER_NEWS_TIME_OUT_IN_SECONDS)
+        if(user_id):
+            redis_client.set(user_id, pickle.dumps(total_news_digests))
+            redis_client.expire(user_id, USER_NEWS_TIME_OUT_IN_SECONDS)
 
         sliced_news = total_news[begin_index: end_index]
 
