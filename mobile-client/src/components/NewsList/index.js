e32844810f35aa0e6f185a31509f51c1cd44ebd2
yuchiu <yuchiu2002@hotmail.com>
2018-11-23 16:23:09 -0500

    setup layout, added scroll view

--- mobile-client/src/components/NewsList/index.js
+++ mobile-client/src/components/NewsList/index.js
@@ -1,10 +1,28 @@
 import React from "react";
-import { FlatList, ActivityIndicator, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  ScrollView,
+  StatusBar,
+  Text,
+  Linking,
+  Dimensions,
+  SafeAreaView,
+  Image,
+  View
+} from "react-native";
+
+const { height } = Dimensions.get("window");
 
 export default class FetchExample extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isLoading: true, newsList: [] };
+    this.state = {
+      isLoading: true,
+      newsList: [],
+      // We don't know the size of the content initially, and the probably won't instantly try to scroll, so set the initial content height to 0
+      screenHeight: 0
+    };
   }
 
   componentDidMount() {
@@ -23,9 +41,15 @@ export default class FetchExample extends React.Component {
       console.log(err);
     }
   };
+  onContentSizeChange = (contentWidth, contentHeight) => {
+    // Save the content height in state
+    this.setState({ screenHeight: contentHeight });
+  };
 
   render() {
     const { isLoading, newsList } = this.state;
+    const scrollEnabled = this.state.screenHeight > height;
+
     if (isLoading) {
       return (
         <View style={{ flex: 1, padding: 20 }}>
@@ -35,13 +59,53 @@ export default class FetchExample extends React.Component {
     }
 
     return (
-      <View style={{ flex: 1, paddingTop: 20 }}>
+      <SafeAreaView style={styles.newsListWrapper}>
         {newsList.map((news, i) => (
-          <View key={"news-list-index-" + i}>
-            <Text>{news.title}</Text>
-          </View>
+          <ScrollView
+            style={styles.newsWrapper}
+            key={"news-list-index-" + i}
+            scrollEnabled={scrollEnabled}
+            onContentSizeChange={this.onContentSizeChange}
+          >
+            <StatusBar barStyle="light-content" backgroundColor="#468189" />
+
+            <Image
+              style={styles.thumbNailImg}
+              source={{ uri: news.urlToImage }}
+            />
+            <Text style={styles.title}>{news.title}</Text>
+            <Text style={styles.description}>{news.description}</Text>
+            <Text style={styles.publishAt}>{news.publishAt}</Text>
+            <Text style={styles.source}>{news.source}</Text>
+            <Text style={styles.url} onPress={() => Linking.openURL(news.url)}>
+              Link to Article
+            </Text>
+            <Text>---------------</Text>
+          </ScrollView>
         ))}
-      </View>
+      </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  newsListWrapper: {
+    width: "90%",
+    borderColor: "grey"
+  },
+  newsWrapper: {
+    flexGrow: 1
+  },
+  thumbNailImg: {
+    alignSelf: "center",
+    height: 150,
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 2
+  },
+  title: { fontWeight: "bold" },
+  description: {},
+  publishAt: {},
+  source: { fontWeight: "bold" },
+  url: { color: "blue" }
+});
